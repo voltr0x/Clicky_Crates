6e7e81e6936dd17bd664024e2955ad54f67c9c26
voltr0x <virajshirodkar09@gmail.com>
2020-02-13 10:30:15 +0530

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private GameObject titleScreen;
+    
+    private float spawnRate = 1.0f;
+    private int score;
+    private int lives = 3;
+    public bool gameActive;
+    
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
+    public TextMeshProUGUI gameoverText;
+    public Button restartButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleScreen = GameObject.Find("Title Screen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTargets()
+    {
+        while (gameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateLives(int livesToSub)
+    {
+        lives -= livesToSub;
+        livesText.text = "Lives: " + lives;
+
+        if (lives == 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        gameoverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        
+        gameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        //Called when any of the difficulty buttons are clicked
+        StartCoroutine(SpawnTargets());
+        UpdateScore(0);
+        UpdateLives(0);
+
+        //Changes difficulty as per button pressed
+        spawnRate /= difficulty;
+
+        gameActive = true;
+
+        //Removes title screen as game starts
+        titleScreen.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    public ParticleSystem explotionParticle;
+
+    private float speedMin = 12;
+    private float speedMax = 16;
+    private float torque = 10;
+    private float xRange = 4;
+    private float yPos = -2;
+
+    public int pointValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        RandomForceGenerator();
+        RandomTorqueGenerator();
+        RandomPositionGenerator();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RandomForceGenerator()
+    {
+        targetRb.AddForce(Vector3.up * Random.Range(speedMin, speedMax), ForceMode.Impulse);
+    }
+
+    void RandomTorqueGenerator()
+    {
+        targetRb.AddTorque(Random.Range(-torque,torque), Random.Range(-torque,torque), Random.Range(-torque,torque), ForceMode.Impulse);
+    }
+    
+    void RandomPositionGenerator()
+    {
+        transform.position = new Vector3(Random.Range(-xRange, xRange), yPos);
+    }
+
+    //Destroy on mouse click
+    private void OnMouseDown()
+    {
+        if (gameManager.gameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+
+            Instantiate(explotionParticle, transform.position, explotionParticle.transform.rotation);
+        }
+    }
+
+    //Destroy fallen objects
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Sensor"))
+        {
+            Destroy(gameObject);
+            if (!gameObject.CompareTag("Bad"))
+            {
+                if (gameManager.gameActive)
+                {
+                    gameManager.UpdateLives(1);
+                }
+            }
+        }
+    }
+}
